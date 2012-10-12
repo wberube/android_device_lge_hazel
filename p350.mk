95f582f623162c30393d90a476720024aa485f9e
RonGokhle <cips173@gmail.com>
2012-10-12 13:48:44 +0530

    p350: flags,flags bla,bla
    Change-Id: Ia8d1393ebcb28d4789520bef2c3abd8ac5b437cb

--- p350.mk
+++ p350.mk
@@ -67,6 +67,8 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full_base.mk)
 
+PRODUCT_LOCALES := en_GB
+
 # Use dalvik parameters for a 512 MB device
 $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 
@@ -124,29 +126,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.camera=p350 \
-    debug.camcorder.disablemeta=1 \
-    ro.com.google.locationfeatures=1 \
-    ro.com.google.networklocation=1 \
-    ro.com.google.gmsversion=2.3_r6 \
-    ro.setupwizard.enable_bypass=1 \
-    dalvik.vm.lockprof.threshold=500 \
-    dalvik.vm.dexopt-flags=m=y \
-    ro.telephony.call_ring.multiple=false \
-    ro.vold.umsdirtyratio=20
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    com.qc.hardware=true \
-    com.qc.hdmi_out=false \
-    debug.sf.hw=1 \
-    debug.enabletr=false \
-    debug.composition.type=mdp \
-    debug.gr.numframebuffers=2 \
-    debug.qctwa.statusbar=1 \
-    debug.qctwa.preservebuf=1 \
-    hwui.render_dirty_regions=false \
-    hwui.disable_vsync=true \
-    hwui.print_config=choice \
-    persist.sys.strictmode.visual=false
+    debug.camcorder.disablemeta=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.enable-player=true \
