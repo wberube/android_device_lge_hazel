f92704674547de390afd367dc8189091dcde99dd
RonGokhle <cips173@gmail.com>
2012-10-13 14:14:20 +0530

    p350: use rild blobs
    Change-Id: I7135517581b240555d3db2f9541b76fdbe0a58dc

--- Android.mk
+++ Android.mk
@@ -2,11 +2,12 @@ LOCAL_PATH := $(my-dir)
 
 ifeq ($(TARGET_DEVICE),p350)
     subdir_makefiles := \
+         $(LOCAL_PATH)/apversion/android.mk \
+         $(LOCAL_PATH)/hwaddrs/android.mk \
 	 $(LOCAL_PATH)/libaudio/Android.mk \
          $(LOCAL_PATH)/libcamera/Android.mk \
          $(LOCAL_PATH)/libcopybit/android.mk \
          $(LOCAL_PATH)/liblights/android.mk \
-         $(LOCAL_PATH)/libril/android.mk \
 	 $(LOCAL_PATH)/source/libomxcore/Android.mk \
          $(LOCAL_PATH)/source/librpc/Android.mk \
 	 $(LOCAL_PATH)/source/libstagefrighthw/Android.mk \
