331af2ae3d793b4cbf7b7a979fac9a58e18dbd7c
RonGokhale <cips173@gmail.com>
2012-10-30 22:59:41 +0530

    p350: clean up
    Change-Id: I72a97881fa69ff75a26e919f842181c651038a3f

--- p350.mk
+++ p350.mk
@@ -123,9 +123,30 @@ PRODUCT_PACKAGES += \
     hciconfig \
     hwaddrs
 
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.camera=p350 \
-    debug.camcorder.disablemeta=1
+    debug.camcorder.disablemeta=1 \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.3_r6 \
+    ro.setupwizard.enable_bypass=1 \
+    ro.telephony.call_ring.multiple=false \
+    ro.vold.umsdirtyratio=20
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    com.qc.hardware=true \
+    com.qc.hdmi_out=false \
+    debug.sf.hw=1 \
+    debug.enabletr=false \
+    debug.composition.type=mdp \
+    debug.gr.numframebuffers=2 \
+    debug.qctwa.statusbar=1 \
+    debug.qctwa.preservebuf=1 \
+    hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true \
+    hwui.print_config=choice \
+    persist.sys.strictmode.visual=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.enable-player=true \
