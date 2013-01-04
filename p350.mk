d38a5af1f8f83c37202e9bb9595f0a147c1f3ca1
RonGokhale <cips173@gmail.com>
2013-01-04 13:06:12 +0530

    p350: add sysctl.conf from ics branch
    Change-Id: Ie3fa4b4ca71dd99e0f56a9f023b0a4472aaf7725

--- p350.mk
+++ p350.mk
@@ -41,7 +41,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
     $(LOCAL_PATH)/configs/qwerty.kl:system/usr/keylayout/qwerty.kl \
     $(LOCAL_PATH)/configs/touch_mcs7000.kl:system/usr/keylayout/touch_mcs7000.kl \
-    $(LOCAL_PATH)/configs/AVRCP.kl:system/usr/keylayout/AVRCP.kl
+    $(LOCAL_PATH)/configs/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    $(LOCAL_PATH)/configs/sysctl.conf:system/etc/sysctl.conf
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
