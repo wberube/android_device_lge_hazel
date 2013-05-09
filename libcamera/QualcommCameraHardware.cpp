a5216657852ce2ebfe7223b75524bd99e179e6bd
Vijay kumar Tumati <vtumati@codeaurora.org>
2013-05-09 20:02:21 +0530

    Camera: Initialization of 'SUPPORTED_VIDEO_SIZES'
    why: Related CTS testcases fails
    without this initialization.
    
    Change-Id: I0c4ee22a8bc6f24d3ac70b34f634289d34dbcbe4

--- libcamera/QualcommCameraHardware.cpp
+++ libcamera/QualcommCameraHardware.cpp
@@ -1277,6 +1277,10 @@ void QualcommCameraHardware::initDefaultParameters()
 
     mParameters.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
                     preview_size_values.string());
+
+    mParameters.set(CameraParameters::KEY_SUPPORTED_VIDEO_SIZES,
+                            preview_size_values.string());
+
     mParameters.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
                     picture_size_values.string());
     mParameters.set(CameraParameters::KEY_SUPPORTED_ANTIBANDING,
